c5194201518daa136a3d5325e749da6294784f6f
JhonHurtado <jhonse2001@gmail.com>
2023-03-30 16:57:41 -0500

    FIRST COMMIT

---
+++ app/src/main/java/com/innovation/myapp/frutas.java
@@ -0,0 +1,14 @@
+package com.innovation.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frutas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frutas);
+    }
+}
